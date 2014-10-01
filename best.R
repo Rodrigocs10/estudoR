c0b5e3fd487f6fb38f5bf5140b7151cb67fc029e
Rodrigo Cabral <rodrigocs10@gmail.com>
2014-10-01 20:03:42 -0300

    Atualização de script

--- best.R
+++ best.R
@@ -1,16 +1,52 @@
 best <- function(state, outcome) {
-	states <- read.csv("hospital-data.csv")
-	outcome <- read.csv("outcome-of-care-measures.csv")
-	if(!any(unique(states[,"State"]) == state)){
+	states <- read.csv("hospital-data.csv", colClasses = "character")
+	outcomes <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	if(is.na(state) | !any(states[,"State"] == state)){
 		stop("invalid state")
 	}
+	if(is.na(outcome) | !any(c(11, 23, 17) == returnNumberOutcome(outcome))){
+		stop("Invalid outcome")
+	}
+
+	numberOutcome <- returnNumberOutcome(outcome)
+	## Outcomes by State
+	outcomeByStates <- split(outcomes, outcomes[,7])
+
+	## Considerando o total
+	##saomenores <- as.numeric(outcome[,11]) ==  min(as.numeric(outcome[,11]))
+
+	##pega somente os de AL 
+	somenteEstado <- outcomeByStates[[state]]
+
+	somenteNomesEstado <- somenteEstado[[2]][!is.na(as.numeric(somenteEstado[[numberOutcome]]))]
+	somenteOutcomeEstado <- as.numeric(somenteEstado[[numberOutcome]][!is.na(as.numeric(somenteEstado[[numberOutcome]]))])
+	
+
+	saomenores <- somenteOutcomeEstado ==  min(somenteOutcomeEstado)
+	sortedResults <- sort(somenteNomesEstado[saomenores])
+	sortedResults[1]
+
 ## Read outcome data
 ## Check that state and outcome are valid
 ## Return hospital name in that state with lowest 30-day death
 ## rate
-summary(outcome)
+#summary(outcome)
 
 
+}
+
+returnNumberOutcome <- function(outcomeName){
+	retorno <- 0
+	if(outcomeName == "heart attack"){
+		retorno <- 11
+	}
+	if(outcomeName == "pneumonia"){
+		retorno <- 23
+	}
+	if(outcomeName == "heart failure"){
+		retorno <- 17
+	}
+	retorno
 }
 
 
@@ -19,6 +55,33 @@ rankhospital <- function(state, outcome, num = "best") {
 ## Check that state and outcome are valid
 ## Return hospital name in that state with the given rank
 ## 30-day death rate
+
+	states <- read.csv("hospital-data.csv", colClasses = "character")
+	outcomes <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	if(is.na(state) | !any(states[,"State"] == state)){
+		stop("invalid state")
+	}
+	if(is.na(outcome) | !any(c(11, 23, 17) == returnNumberOutcome(outcome))){
+		stop("Invalid outcome")
+	}
+
+	numberOutcome <- returnNumberOutcome(outcome)
+	## Outcomes by State
+	outcomeByStates <- split(outcomes, outcomes[,7])
+
+	## Considerando o total
+	##saomenores <- as.numeric(outcome[,11]) ==  min(as.numeric(outcome[,11]))
+
+	##pega somente os de AL 
+	somenteEstado <- outcomeByStates[[state]]
+	somenteNomesEstado <- somenteEstado[[2]][!is.na(as.numeric(somenteEstado[[numberOutcome]]))]
+	somenteOutcomeEstado <- as.numeric(somenteEstado[[numberOutcome]][!is.na(as.numeric(somenteEstado[[numberOutcome]]))])
+	
+
+	saomenores <- somenteOutcomeEstado ==  min(somenteOutcomeEstado)
+	sortedResults <- sort(somenteNomesEstado[saomenores])
+	sortedResults[1]
+
 }
 
 
